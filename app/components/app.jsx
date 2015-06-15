e163bb51a7b1a0a6efa33d474ec9ed925d4b6593
Benjamin Shyong <ben.shyong@gmail.com>
2015-06-15 00:42:52 -0700

    resolves #1; remove dynamic date since it's causing React checksum issues

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -18,7 +18,7 @@ export default class App extends React.Component {
 
   state = this.props.flux
     .getStore('locale')
-    .getState();
+    .getState()
 
   componentDidMount() {
     this.props.flux
--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -7,7 +7,7 @@ export default class Footer extends React.Component {
     return (
       <footer className="border-top p2">
         <div className='h5 gray'>
-          TrailerZoo {new Date().getFullYear()}
+          TrailerZoo 2015
         </div>
       </footer>
     );
